2dcf9faacb7a9ff56fa7f387fa00f1b28b3d48bc
Eduardo Ferreira <eduardoferreira@icloud.com>
2020-09-30 21:52:14 -0300

    Refactoring some methods

--- backend/src/modules/beer/repositories/IBeerRepository.ts
+++ backend/src/modules/beer/repositories/IBeerRepository.ts
@@ -4,7 +4,10 @@ import ICreateBeerDTO from '../dtos/ICreateBeerDTO';
 export default interface IBeerRepository {
   findBeers(): Promise<Beer[] | undefined>;
   delete(beer_id: string, brewer_id: string): Promise<void>;
-  findBeer(beer_id: string, brewer_id: string): Promise<Beer | undefined>;
+  findBrewerBeers(
+    beer_id: string,
+    brewer_id: string,
+  ): Promise<Beer[] | undefined>;
   create(beerData: ICreateBeerDTO): Promise<Beer>;
   save(beer: Beer): Promise<Beer>;
 }
